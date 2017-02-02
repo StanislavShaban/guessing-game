1e8c3ddb58b97f4c96f33dcf63aba895017de067
Stanislav Shaban <shabanstas@mail.ru>
2017-02-03 00:01:51 +0300

    Guessing game final version

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+    	this.min = 0;
+    	this.max = 0;
+    	this.current = 0;
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+    	var current = this.current = Math.round((this.min + this.max)/2);
+    	return current;
     }
 
     lower() {
-
+    	var current = this.current;
+    	this.max = current;
     }
 
     greater() {
-
+    	var current = this.current;
+    	this.min = current;
     }
 }
 
